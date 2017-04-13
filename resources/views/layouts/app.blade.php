21a0e8fcb352bda656c705db61a9ba1ffaf4d582
Dimi Antoni <antonivargas@gmail.com>
2017-04-13 13:14:03 -0300

    Alterado view de layout, retirado layout e utilizado app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,87 +1,128 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
     <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Styles -->
-    <link href="/css/app.css" rel="stylesheet">
-
+    <!-- CSS  -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="{{ asset('/css/materialize.css') }}" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <link href="{{ asset('/css/style.css') }}" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <!-- Scripts -->
     <script>
         window.Laravel = <?php echo json_encode([
             'csrfToken' => csrf_token(),
-        ]); ?>
+            ]); ?>
     </script>
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @if (Auth::guest())
-                            <li><a href="{{ url('/login') }}">Login</a></li>
-                            <li><a href="{{ url('/register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+        <nav class="white" role="navigation">
+            <div class="nav-wrapper container">
+                <a id="logo-container" href="{{ url('/') }}" class="brand-logo">Seu Político</a>
+                <ul class="right hide-on-med-and-down">
+                    <li><a href="{{ url('/politicos') }}">Políticos</a></li>
+                    <li><a href="{{ url('/#contato') }}">Fale Conosco</a></li>
+                    <!-- Authentication Links -->
+                    @if (Auth::guest())
+                    <li><a href="/login">Entrar</a></li>
+                    <li><a href="{{ url('/register') }}">Cadastre-se</a></li>
+                    @else
+                        <ul id="dropdown1" class="dropdown-content">
+                            <li class="divider"></li>
+                            <li><a href="#!">Perfil</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#!">Configurações</a></li>
+                            <li>
+                                <a href="{{ url('/logout') }}"
+                                    onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                        Sair
                                 </a>
-
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ url('/logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
+                                <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
                             </li>
-                        @endif
-                    </ul>
-                </div>
+                        </ul>
+                        <!-- Dropdown Trigger -->
+                        <li>
+                            <a class="dropdown-button" href="#!" data-activates="dropdown1">{{ Auth::user()->name }} <span class="caret"><i class="material-icons right">arrow_drop_down</i></a>
+                        </li>
+                    @endif
+                </ul>
+
+                <ul id="nav-mobile" class="side-nav">
+                    <li><a href="{{ url('/politicos') }}">Políticos</a></li>
+                    <li><a href="#">Fale Conosco</a></li>
+                    <!-- Authentication Links -->
+                    @if (Auth::guest())
+                    <li><a href="{{ url('/login') }}">Entrar</a></li>
+                    <li><a href="{{ url('/register') }}">Cadastre-se</a></li>
+                    @else
+                        <ul id="dropdown2" class="dropdown-content">
+                            <li class="divider"></li>
+                            <li><a href="#!">Perfil</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#!">Configurações</a></li>
+                            <li>
+                                <a href="{{ url('/logout') }}"
+                                    onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                        Sair
+                                </a>
+                                <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
+                            </li>
+                        </ul>
+                        <!-- Dropdown Trigger -->
+                        <li>
+                            <a class="dropdown-button" href="#!" data-activates="dropdown2">{{ Auth::user()->name }} <span class="caret"><i class="material-icons right">arrow_drop_down</i></a>
+                        </li>
+                    @endif
+                </ul>
+                <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
             </div>
         </nav>
-
         @yield('content')
     </div>
 
-    <!-- Scripts -->
-    <script src="/js/app.js"></script>
+    <footer class="page-footer teal">
+        <div class="container">
+            <div class="row">
+                <div class="col l6 s12">
+                    <h5 class="white-text">GEATI / IFC - Camboriú </h5>
+                    <p class="grey-text text-lighten-4">O GEATI é o Grupo de Estudos Avançados em Tecnologia da Informação do Instituto Federal Catarinense Campus Camboriú.</p>
+                </div>
+                <div class="col l3 s12">
+                    <h5 class="white-text">Conecte-se</h5>
+                    <ul>
+                        <li><a class="white-text" href="http://www.camboriu.ifc.edu.br/">IFC - Camboriú</a></li>
+                        <li><a class="white-text" href="http://www.geati.ifc-camboriu.edu.br/">GEATI - Camboriú</a></li>
+                        <li><a class="white-text" href="https://github.com/GEATI-IFC">Github GEATI</a></li>
+                    </ul>
+                </div>
+                <div class="col l3 s12">
+                    <h5 class="white-text">Links Úteis</h5>
+                    <ul>
+                        <li><a class="white-text" href="http://www2.camara.leg.br/">Câmara</a></li>
+                        <li><a class="white-text" href="http://www.senado.leg.br/">Senado</a></li>
+                        <li><a class="white-text" href="http://dados.gov.br/">Dados Abertos</a></li>
+                        <li><a class="white-text" href="http://vigieaqui.com.br/">Vigie Aqui</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <div class="container">
+                Desenvolvido por <a class="brown-text text-lighten-3" href="http://www.geati.ifc-camboriu.edu.br/"> GEATI / IFC - Camboriú</a>
+            </div>
+        </div>
+    </footer>
+
+    <!--  Scripts-->
+    <script src="{{ asset('/js/materialize.js') }}"></script>
+    <script src="{{ asset('/js/init.js') }}"></script>
 </body>
 </html>
